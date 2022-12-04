a1701e1ede56ebf2c417a4f40e837e4b90b76099
erunan-josu <josu.hernandez92@gmail.com>
2022-12-04 21:55:35 +0100

    feat: add vanilla+css and vanilla+tailwind templates

---
+++ src/templates/vanilla/template-vanilla-tailwind-vitest/main.js
@@ -0,0 +1,7 @@
+import './style.css'
+
+document.querySelector('#app').innerHTML = `
+<div class='min-h-screen flex justify-center items-center bg-gray-800'>
+<h1 class='text-purple-600' >Vite + Vanilla + Tailwind + Vitest</h1>
+</div>
+`
