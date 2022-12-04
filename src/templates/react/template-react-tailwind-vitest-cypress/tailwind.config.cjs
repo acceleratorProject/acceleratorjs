81fae6713f6df377e589ae5f625c344ad44a3caa
Josu Hernandez <josu.hernandez92@gmail.com>
2022-12-04 16:07:55 +0100

    feat: add react + tailwind templates

---
+++ src/templates/react/template-react-tailwind-vitest-cypress/tailwind.config.cjs
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    colors: {
+      defaultBg: '#242424',
+      text: '#6B43B2'
+    },
+    extend: {}
+  },
+  plugins: []
+}
